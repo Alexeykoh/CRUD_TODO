482b124ed7d14d7c75a7500a95d51707362f477d
ivan <ivan@DESKTOP-NELLSAH>
2023-06-13 00:29:59 +0300

    init: new server project

---
+++ CRUD_TODO_SERVER/Controllers/LOGINController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD_TODO_SERVER.Controllers
+
+{
+      [ApiController]
+      [Route("[controller]")]
+    public class LOGINController : ControllerBase
+    {
+        
+            [HttpGet]
+
+            public string Login(string login,string password)
+            {
+                return "kek";
+            }
+      
+       
+        
+        
+    }
+}
