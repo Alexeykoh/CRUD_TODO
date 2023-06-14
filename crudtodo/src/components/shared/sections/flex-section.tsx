fdc91be2081f7401192abc7adf5b22f0141306dd
Alexeykoh <Alexeykoh@icloud.com>
2023-06-14 17:41:16 +0300

    fix: fix padding

--- crudtodo/src/components/shared/sections/flex-section.tsx
+++ crudtodo/src/components/shared/sections/flex-section.tsx
@@ -7,7 +7,7 @@ interface FlexSectionProps {
 export const FlexSection: React.FC<FlexSectionProps> = props => {
     return (
         <>
-            <div className={'flex flex-col w-full p-2 bg-white rounded-md'}>
+            <div className={'flex flex-col w-full py-2 bg-white rounded-md'}>
                 {props.children}
             </div>
         </>
