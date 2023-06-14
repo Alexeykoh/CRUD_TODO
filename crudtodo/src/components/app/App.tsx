77caa78e4a00adf11c9f0dfb14550c7d15796725
Alexeykoh <Alexeykoh@icloud.com>
2023-06-14 17:40:56 +0300

    featL redirect if localstorage is empty

--- crudtodo/src/components/app/App.tsx
+++ crudtodo/src/components/app/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../../App.css';
-import {Route, Routes} from "react-router";
+import {Route, Routes, useNavigate} from "react-router";
 import {AuthPage} from "../pages/auth-page/auth-page";
 import {ErrorPage} from "../pages/error-page/error-page";
 import {RegistrationPage} from "../pages/registration-page/registration-page";
@@ -9,12 +9,25 @@ import {UserPage} from "../pages/user-page/user-page";
 import {Layout} from "./layout";
 
 function App() {
+    const navigate = useNavigate();
+
+    function checkIsLogin(){
+        const localLoginData = localStorage.getItem('loginUser')
+        if(localLoginData===null){
+            navigate('/auth')
+        }
+    }
+
+    useEffect(() => {
+        checkIsLogin()
+    }, []);
+
+
     return (
         <Routes>
             <Route path="/" element={<Layout/>}>
                 <Route index element={<TodoPage/>}/>
                 <Route path="auth" element={<AuthPage/>}/>
-                <Route path="registration" element={<RegistrationPage/>}/>
                 <Route path="user" element={<UserPage/>}/>
                 <Route path="*" element={<ErrorPage/>}/>
             </Route>
