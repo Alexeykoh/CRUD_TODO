9cbd740ed10b9842cdfa83ab83d2c5e248ec31ed
Alexeykoh <Alexeykoh@icloud.com>
2023-06-14 17:41:30 +0300

    refactor: change bg color

--- crudtodo/src/components/shared/labels/greeting-features-label.tsx
+++ crudtodo/src/components/shared/labels/greeting-features-label.tsx
@@ -9,7 +9,7 @@ interface GreetingFeaturesLabelProps {
 export const GreetingFeaturesLabel: React.FC<GreetingFeaturesLabelProps> = props => {
     return (
         <>
-            <div className="py-16 px-10 bg-sky-300 rounded-lg w-1/3 h-full flex flex-col">
+            <div className="py-16 px-10 bg-black/75 rounded-lg w-1/3 h-full flex flex-col">
                 <Logo logo={iconLogo}/>
                 <div className="flex flex-col gap-8 pt-32 text-white ">
                     <h2 className={'text-5xl font-semibold'}>Планируй свой день вместе с ToDo</h2>
